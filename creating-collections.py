c7055973c9c2440e13b4086d85132b6a349548f7
ahmetozmtn <ahmethack13@gmail.com>
2023-12-17 19:49:29 +0300

    mongodb multiple data insertion

--- creating-collections.py
+++ creating-collections.py
@@ -21,3 +21,16 @@ mycollection = mydb["products"]
 
 # db'deki collectionları  listeler
 # print(mydb.list_collection_names())
+
+
+productList = [
+    {"name": "Iphone 15", "price": "60000", "description": "Color Black"},
+    {"name": "Iphone 14", "price": "50000", "description": "Color Black",
+        "categories": ["telefon", "elektronik"]},
+    {"name": "Iphone 13", "price": "40000", "description": "Color Black"},
+    {"name": "Iphone 12", "price": "30000", "description": "Color Black"},
+    {"name": "Iphone 11", "price": "20000", "description": "Color Black"}
+]
+
+result = mycollection.insert_many(productList)
+print(result.inserted_ids)
