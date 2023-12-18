9eb99e0f1eb348f5121da70ab55d6da105441bfb
ahmetozmtn <ahmethack13@gmail.com>
2023-12-18 21:36:21 +0300

    Mongodb sorts by name.

--- mongdb-sorting.py
+++ mongdb-sorting.py
@@ -7,7 +7,14 @@ myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient["demo"]
 mycollection = mydb["products"]
 
-result = mycollection.find().sort("name")  # name göre sıralama yapar.
+result = mycollection.find().sort("name")  # name göre artan sıralama yapar.
+
+# name göre azalan sıralama yapar.
+result = mycollection.find().sort("name", -1)
+
+
+result = mycollection.find().sort("price")  # artan fiyata göre sıralar
+result = mycollection.find().sort("price", -1)  # azalan fiyata göre sıralar
 
 for i in result:
     print(i)
