c4369526d87c240efdc8de8df3f19a51d258214f
ahmetozmtn <ahmethack13@gmail.com>
2023-12-18 17:02:11 +0300

    query by mongodb id

--- filter-records.py
+++ filter-records.py
@@ -1,4 +1,6 @@
 import pymongo
+from bson.objectid import ObjectId
+
 
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 
@@ -8,5 +10,7 @@ mycollection = mydb["products"]
 filter = {"name": "Iphone 11"}
 
 result = mycollection.find_one(filter)  # name göre sorgulama
+result = mycollection.find_one(
+    {"_id": ObjectId("657f1a16b697374df9620456")})  # id'ye göre sorgulama
 
 print(result)
