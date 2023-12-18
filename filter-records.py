71c51f6ab28560e88e409b5c2af5c7074d48e966
ahmetozmtn <ahmethack13@gmail.com>
2023-12-18 17:09:44 +0300

    Mongodb "$in" operator usage

--- filter-records.py
+++ filter-records.py
@@ -7,10 +7,17 @@ myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient["demo"]
 mycollection = mydb["products"]
 
-filter = {"name": "Iphone 11"}
 
-result = mycollection.find_one(filter)  # name göre sorgulama
+result = mycollection.find_one({"name": "Iphone 11"})  # name göre sorgulama
 result = mycollection.find_one(
     {"_id": ObjectId("657f1a16b697374df9620456")})  # id'ye göre sorgulama
 
-print(result)
+result = mycollection.find({
+    "name": {
+        # dizi de belirtilenden biri varsa return eder
+        "$in": ["Iphone 15", "Iphone 16"]
+    }
+})
+
+for i in result:
+    print(i)
